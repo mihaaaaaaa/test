d6a5ae8844a7b74b72604df53f14e30ef732e92d
bananaWeb <a89.volkov@gmail.com>
2018-03-12 18:51:44 +0300

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,56 @@
 $(document).ready(function() {
+
 	let backgrounds = [
 	'bg1.png', 
 	'bg2.png'
 	]
+
 	let links = [
 	'.modal-links a:first-child',
 	'.modal-links a:nth-child(2)',
 	'.modal-links a:last-child'
 	]
+
 	$('#particles-js').css({
 		'background-image':'url(./assets/images/' + backgrounds[Math.floor(Math.random() * (2 - 0) + 0)] + ')'
 	});
+
 	let mediaToggler = $('.js-modal-toggler'),
 		modal = $('.modal'),
-		mobileNav = $('.mobile-nav');
+		mobileNav = $('.mobile-nav'),
+		modalElements = [
+			'.js-icon-github',
+			'.js-icon-vk',
+			'.js-icon-mail',
+			'.modal-map',
+			'.modal-toggler'
+		];
 
 
-	$('.nav-toggler').click(function(){
-		mobileNav.toggleClass('-is-open');
+	mobileNav.click(function($event) {
+		event.preventDefault();
+		if (modal.hasClass('-active')) {
+			modal.removeClass('-active');
+		}
+		else {
+			mobileNav.toggleClass('-is-open');
+		};
 	});
 
 	setTimeout(function(){
 		$('.section-main h1').addClass('-animated');
 		$('.section-main h2').addClass('-animated');
 	},1000);
-		mediaToggler.click(function($event){
+
+	mediaToggler.click(function($event){
 		event.preventDefault();
 		modal.toggleClass('-active');
+		for (let i = 0; i < modalElements.length; i++) {
+			const delay = 200 * i;
+			
+			setTimeout(function() {
+				$(modalElements[i]).toggleClass('-animated');
+			}, 200 + delay)
+		}
 	})
-	for (var i = 1; i < 4; i++) {
-		setTimeout(function(){
-		$('.links[i]').toggleClass('-animated');
-		},1 + i);
-	}
-	mediaToggler.click(function(){
-	setTimeout(function(){
-		$('.modal-map').toggleClass('-animated')
-	},2000);
-	})
-
 });
